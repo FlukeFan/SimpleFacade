0f63ba419e85f67b552b3ef1e846f94966d8a8b7
Richard Brown <flukefan@googlemail.com>
2017-11-30 17:22:18 +0000

    Added compatibility analyser and treated warnings as errors

---
+++ SimpleFacade.Tests/DesignContraintsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace SimpleFacade.Tests
+{
+    [TestFixture]
+    public class DesignContraintsTests
+    {
+        [Test]
+        public void DependenciesHaveNotChanged()
+        {
+            var folder = @"..\..\..\..\SimpleFacade\bin";
+            var name = "SimpleFacade";
+
+            NugetPackage.VerifyDependencies(folder, name, new string[]
+            {
+                "System.ComponentModel.Annotations:*",
+            });
+        }
+
+        [Test]
+        public void Testing_DependenciesHaveNotChanged()
+        {
+            var folder = @"..\..\..\..\SimpleFacade.Testing\bin";
+            var name = "SimpleFacade.Testing";
+
+            NugetPackage.VerifyDependencies(folder, name, new string[]
+            {
+                "SimpleFacade:*",
+            });
+        }
+    }
+}
